6c3ff0ee1440327982a1f77a707804dac418b4e5
Sungho Park <siminsungho@gmail.com>
2019-09-10 15:56:00 +0900

    Refactoring

--- app/myble_app_multilink_central2_nrf52dk.cmake
+++ app/myble_app_multilink_central2_nrf52dk.cmake
@@ -1,10 +1,9 @@
-set_cache(NRF5SDK__BOARD_NAME                                                   "PCA10040"  STRING)
-set_cache(NRF5SDK__BOARD_CONFIG_NAME                                            "BLE_APP_MULTILINK_CENTRAL"    STRING)
+set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_ORIGIN 0x20004400 STRING)
+set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_LENGTH 0x0000BC00 STRING)
 
-set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_ORIGIN                                   0x20004400     STRING)
-set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_LENGTH                                   0x0000BC00     STRING)
-
-set_cache(NRF5SDK__SWI_DISABLE0                                                 TRUE    BOOL)
+set_cache(NRF5SDK__BOARD_NAME "PCA10040" STRING)
+set_cache(NRF5SDK__BOARD_CONFIG_NAME "BLE_APP_MULTILINK_CENTRAL" STRING)
+set_cache(NRF5SDK__SWI_DISABLE0 TRUE BOOL)
 
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52dk_softdevice.cmake)
 
@@ -12,8 +11,8 @@ include(${PROJECT_LIBRARY_DIR}/nrf5sdk/config/nrf5sdk.cmake)
 
 ####
 
-set(INCLUDE__APP                                                                TRUE)
-set(APP__NAME                                                                   "myble_app_multilink_central2")
+set(INCLUDE__APP TRUE)
+set(APP__NAME "myble_app_multilink_central2")
 
 get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/myble_app_multilink_central2" ABSOLUTE)
 
@@ -24,5 +23,4 @@ file(GLOB_RECURSE _tmp_sources
     "${_tmp_source_dir}/*.s")
 
 set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
-
 
--- app/myrtc2_nrf52dk.cmake
+++ app/myrtc2_nrf52dk.cmake
@@ -1,7 +1,6 @@
-set_cache(NRF5SDK__BOARD_NAME                                                   "PCA10040"  STRING)
-set_cache(NRF5SDK__BOARD_CONFIG_NAME                                            "RTC"       STRING)
-
-set_cache(NRF5SDK__BSP_DEFINES_ONLY                                             TRUE    BOOL)
+set_cache(NRF5SDK__BOARD_NAME "PCA10040" STRING)
+set_cache(NRF5SDK__BOARD_CONFIG_NAME "RTC"STRING)
+set_cache(NRF5SDK__BSP_DEFINES_ONLY TRUE BOOL)
 
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52dk.cmake)
 
@@ -9,8 +8,8 @@ include(${PROJECT_LIBRARY_DIR}/nrf5sdk/config/nrf5sdk.cmake)
 
 ####
 
-set(INCLUDE__APP                                                                TRUE)
-set(APP__NAME                                                                   "myrtc2")
+set(INCLUDE__APP TRUE)
+set(APP__NAME "myrtc2")
 
 get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/myrtc2" ABSOLUTE)
 
@@ -21,5 +20,4 @@ file(GLOB_RECURSE _tmp_sources
     "${_tmp_source_dir}/*.s")
 
 set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
-
 
